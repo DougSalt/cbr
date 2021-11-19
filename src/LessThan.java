95af232c11155c5488b39c686f9391f98c4490af
Doug Salt <doug.salt@hutton.ac.uk>
2021-11-19 13:04:24 +0000

    Code review changes (and because Windows is crap at tracking files)

---
+++ src/LessThan.java
@@ -0,0 +1,19 @@
+import org.nlogo.api.Reporter;
+import org.nlogo.api.*;
+import org.nlogo.core.*;
+
+public class LessThan implements Reporter
+{
+	@Override
+	public Object report(Argument[] args, Context context)
+		throws ExtensionException
+	{
+		return CaseBase.YES;
+	}
+
+	@Override
+	public Syntax getSyntax()
+	{
+		return SyntaxJ.reporterSyntax(Syntax.StringType());
+	}
+}
